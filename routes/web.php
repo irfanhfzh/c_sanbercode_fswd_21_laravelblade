83c62c0b6a74bfe00b1d1a5e42a59d9a8731a4bd
irfanhfzh <irfanhafizh53@gmail.com>
2021-01-28 09:17:17 +0700

    Pekan 3 - Hari 3 - Memasangkan Template dengan Laravel Blade

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/master', function(){
+    return view('master');
 });
